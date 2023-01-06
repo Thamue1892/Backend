fb6e570561052e3203d510f8906b3e0d107bb61e
Thamsanqa <thamsanqa@tirisan.co.za>
2023-01-06 16:23:46 +0200

    Implemented the GET Requests.

--- Repository/CompanyRepository.cs
+++ Repository/CompanyRepository.cs
@@ -8,9 +8,15 @@ using Entities.Models;
 
 namespace Repository
 {
-    public class CompanyRepository:RepositoryBase<Company>,ICompanyRepository
+    public class CompanyRepository : RepositoryBase<Company>, ICompanyRepository
     {
         public CompanyRepository(RepositoryContext repositoryContext)
-            :base(repositoryContext) { }
+            : base(repositoryContext) { }
+
+        public IEnumerable<Company> GetAllCompanies(bool trackChanges) =>
+        FindAll(trackChanges)
+            .OrderBy(c => c.Name)
+        .ToList();
+
     }
 }
--- Service/CompanyService.cs
+++ Service/CompanyService.cs
@@ -4,19 +4,34 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Contracts;
+using Entities.Models;
 using Service.Contracts;
 
 namespace Service
 {
-    internal sealed class CompanyService:ICompanyService
+    internal sealed class CompanyService : ICompanyService
     {
         private readonly IRepositoryManager _repository;
         private readonly ILoggerManager _logger;
 
-        public CompanyService(IRepositoryManager repository,ILoggerManager logger)
+        public CompanyService(IRepositoryManager repository, ILoggerManager logger)
         {
             _repository = repository;
             _logger = logger;
+        }
+
+        public IEnumerable<Company> GetAllCompanies(bool trackChanges)
+        {
+            try
+            {
+                var companies = _repository.Company.GetAllCompanies(trackChanges);
+                return companies;
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"Something went wrong in the {nameof(GetAllCompanies)} service method {ex}");
+                throw;
+            }
         }
     }
 }
