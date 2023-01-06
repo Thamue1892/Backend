9be038447fe31abe3816f3b4f42a85d14deba2e3
Thamsanqa <thamsanqa@tirisan.co.za>
2023-01-06 17:05:07 +0200

    Implemented CompanyDto.

--- Service.Contracts/ICompanyService.cs
+++ Service.Contracts/ICompanyService.cs
@@ -4,11 +4,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Entities.Models;
+using Shared.DataTransferObjects;
 
 namespace Service.Contracts
 {
     public interface ICompanyService
     {
-        IEnumerable<Company> GetAllCompanies(bool trackChanges);
+        IEnumerable<CompanyDto> GetAllCompanies(bool trackChanges);
     }
 }
