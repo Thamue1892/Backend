50aa7cb466c3ffa9024604ee61cd6793f5bd0d0f
Thamsanqa <thamue1892@gmail.com>
2023-01-07 23:50:44 +0200

    Implemented getting all employees for a given company.

--- Repository/EmployeeRepository.cs
+++ Repository/EmployeeRepository.cs
@@ -8,12 +8,17 @@ using Entities.Models;
 
 namespace Repository
 {
-    public class EmployeeRepository:RepositoryBase<Employee>,IEmployeeRepository
+    public class EmployeeRepository : RepositoryBase<Employee>, IEmployeeRepository
     {
         public EmployeeRepository(RepositoryContext repositoryContext)
-        :base(repositoryContext)
+        : base(repositoryContext)
         {
-            
+
         }
+
+        public IEnumerable<Employee> GetEmployees(Guid companyId, bool trackChanges) =>
+            FindByCondition(e => e.CompanyId.Equals(companyId), trackChanges)
+                .OrderBy(e => e.Name).ToList();
+
     }
 }
--- Service/EmployeeService.cs
+++ Service/EmployeeService.cs
@@ -5,21 +5,34 @@ using System.Text;
 using System.Threading.Tasks;
 using AutoMapper;
 using Contracts;
+using Entities.Exceptions;
 using Service.Contracts;
+using Shared.DataTransferObjects;
 
 namespace Service
 {
-    internal sealed class EmployeeService:IEmployeeService
+    internal sealed class EmployeeService : IEmployeeService
     {
         private readonly IRepositoryManager _repository;
         private readonly ILoggerManager _logger;
         private readonly IMapper _mapper;
 
-        public EmployeeService(IRepositoryManager repository,ILoggerManager logger,IMapper mapper)
+        public EmployeeService(IRepositoryManager repository, ILoggerManager logger, IMapper mapper)
         {
             _repository = repository;
             _logger = logger;
             _mapper = mapper;
+        }
+
+        public IEnumerable<EmployeeDto> GetEmployees(Guid companyId, bool trackChanges)
+        {
+            var company = _repository.Company.GetCompany(companyId, trackChanges);
+            if (company is null) throw new CompanyNotFoundException(companyId);
+
+            var employeesFromDb = _repository.Employee.GetEmployees(companyId, trackChanges);
+            var employeesDto = _mapper.Map<IEnumerable<EmployeeDto>>(employeesFromDb);
+
+            return employeesDto;
         }
     }
 }
